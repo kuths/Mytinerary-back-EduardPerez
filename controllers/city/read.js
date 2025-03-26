8402c763ec2196c5e0e32c63624b3fe643c2f7e5
Eduard <ehpr165@gmail.com>
2025-03-26 11:08:34 -0500

    mofdificacion de controllerss

--- controllers/city/read.js
+++ controllers/city/read.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import City from "../../models/city.js";
 
 let allCity = async (req, res, next) => {
@@ -43,37 +42,8 @@ let cityById = async (req, res, next) => {
         next(error);
     }
 };
-let updateCity = async (req, res, next) => {
-    try {
-        let cityId = req.params.id;
-        let newData = req.body; 
 
-        if (!mongoose.Types.ObjectId.isValid(cityId)) {
-            return res.status(400).json({ error: "Bad request" });
-        }
 
-        let updatedCity = await City.findByIdAndUpdate(cityId, newData, { new: true });
 
-        return res.status(200).json({ response: updatedCity });
-    } catch (error) {
-        next(error);
-    }
-};
 
-let deleteCity = async (req, res, next) => {
-    try {
-        let cityId = req.params.id;
-
-        if (!mongoose.Types.ObjectId.isValid(cityId)) {
-            return res.status(400).json({ error: "Bad request" });
-        }
-
-        let deletedCity = await City.findByIdAndDelete(cityId);
-
-        return res.status(200).json({ message: "City deleted successfully", response: deletedCity });
-    } catch (error) {
-        next(error);
-    }
-};
-
-export { allCity, cityByName, cityById, updateCity, deleteCity}
+export { allCity, cityByName, cityById,}
